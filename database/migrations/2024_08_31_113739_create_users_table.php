67dbb0bab2643cfef7545cc48b86ada13ff0a833
guilherme_celente <guilherme.celente@outlook.com>
2024-09-12 09:01:06 -0300

    deploy

--- database/migrations/2024_08_31_113739_create_users_table.php
+++ database/migrations/2024_08_31_113739_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('nome', 20);
             $table->string('email', 100);
-            $table->string('senha', 12);
+            $table->string('senha', 300);
             $table->integer('type_id');
             $table->integer('instituicao_id')->nullable();
             $table->timestamps();
