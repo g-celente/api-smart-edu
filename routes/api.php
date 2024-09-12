e3ce2a0ebdda8b0e8e269aea83c14a5b5fe08e40
guilherme_celente <guilherme.celente@outlook.com>
2024-09-12 10:12:40 -0300

    deploy

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/', function () {
+    return 'Olá, você está na API';
+});
+
 
 //ROTAS PARA REGISTRO E LOGIN INSTITUIÇÃO
 Route::post('/register', 'App\Http\Controllers\LoginRegisterController@registerInstituicao');
