2cfee079c98bad45de912739eb6508573f4a3455
guilherme_celente <guilherme.celente@outlook.com>
2024-09-23 09:50:57 -0300

    att

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Instituicoe;
+use Illuminate\Support\Facades\Hash;
 
 class PerfilController extends Controller
 {
@@ -34,5 +35,59 @@ class PerfilController extends Controller
             'type_id' => $usuarios->type_id
         ]);
     }
+
+    public function update_perfil(Request $request) {
+        $user_id = $request->input('authenticated_user_id');
+        
+        $user = User::where('id', $user_id)->first();
+
+        if (!$user) {
+            return response()->json([
+                'error' => 'user não encontrado'
+            ], 404);
+        }
+
+        $user->update([
+            'nome' => $request->nome,
+            'email' => $request->email,
+            'type_id' => $request->type_id,
+            'instituicao_id' => $request->instituicao_id
+        ]);
+        return response()->json([
+            'success' => 'usuário alterado',
+            'user' => $user
+        ], 200);
+    }
+
+    public function update_password(Request $request) {
+        $user_id = $request->input('authenticated_user_id');
+
+        $user = User::where('id', $user_id)->first();
+
+        if (!$user) {
+            return response()->json([
+                'error' => 'usuário não encontrado no banco'
+            ], 404);
+        }
+        
+        try{
+            $user->update([
+                'senha' => Hash::make($request->senha)
+            ]);
+
+            return response()->json([
+                'success' => 'senha alterada',
+                'user' => $user
+            ], 200);
+        }
+        catch (\Exception $e) {
+            return response()->json([
+                'error' => 'erro ao tentar alterar a senha',
+                'problema' => $e
+            ]);
+        }
+        
+    }
+
 
 }
