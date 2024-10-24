b747ad9f05788975384f439f3c3a17a96b05e40b
guilherme_celente <guilherme.celente@outlook.com>
2024-10-24 07:56:18 -0300

    att

--- database/migrations/2024_10_21_124116_create_material_atividades_table.php
+++ database/migrations/2024_10_21_124116_create_material_atividades_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMaterialComplementarsTable extends Migration
+class CreateMaterialAtividadeTable extends Migration
 {
     /**
      * Run the migrations.
